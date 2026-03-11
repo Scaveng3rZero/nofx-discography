219f784907391366c56aba3eebbdf029ad33ff09
Scaveng3r <128704976+Scaveng3rZero@users.noreply.github.com>
2026-03-11 17:21:40 -0400

    Update albums.js

--- js/albums.js
+++ js/albums.js
@@ -1,1 +1,93 @@
+const albums = [
 
+{
+year:1988,
+title:"Liberal Animation",
+cover:"img/covers/1988.jpg"
+},
+
+{
+year:1989,
+title:"S&M Airlines",
+cover:"img/covers/1989.jpg"
+},
+
+{
+year:1991,
+title:"Ribbed",
+cover:"img/covers/1991.jpg"
+},
+
+{
+year:1992,
+title:"White Trash, Two Heebs and a Bean",
+cover:"img/covers/1992.jpg"
+},
+
+{
+year:1994,
+title:"Punk in Drublic",
+cover:"img/covers/1994.jpg"
+},
+
+{
+year:1996,
+title:"Heavy Petting Zoo",
+cover:"img/covers/1996.jpg"
+},
+
+{
+year:1997,
+title:"So Long and Thanks for All the Shoes",
+cover:"img/covers/1997.jpg"
+},
+
+{
+year:2000,
+title:"Pump Up the Valuum",
+cover:"img/covers/2000.jpg"
+},
+
+{
+year:2003,
+title:"The War on Errorism",
+cover:"img/covers/2003.jpg"
+},
+
+{
+year:2006,
+title:"Wolves in Wolves Clothing",
+cover:"img/covers/2006.jpg"
+},
+
+{
+year:2009,
+title:"Coaster",
+cover:"img/covers/2009.jpg"
+},
+
+{
+year:2012,
+title:"Self Entitled",
+cover:"img/covers/2012.jpg"
+},
+
+{
+year:2016,
+title:"First Ditch Effort",
+cover:"img/covers/2016.jpg"
+},
+
+{
+year:2021,
+title:"Single Album",
+cover:"img/covers/2021.jpg"
+},
+
+{
+year:2022,
+title:"Double Album",
+cover:"img/covers/2022.jpg"
+}
+
+];
